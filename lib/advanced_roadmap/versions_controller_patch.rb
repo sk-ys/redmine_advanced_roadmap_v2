04b401186b33a5d79a1fa7cc3ae559c4f06e29fa
David.Ou <gault_1@icloud.com>
2020-07-13 21:02:42 +0900

    Fixed Redmine version 4.1.1.

--- lib/advanced_roadmap/versions_controller_patch.rb
+++ lib/advanced_roadmap/versions_controller_patch.rb
@@ -2,28 +2,46 @@ require_dependency "versions_controller"
 
 module AdvancedRoadmap
   module VersionsControllerPatch
-    def self.included(base)
-      base.class_eval do
-  
-        def index_with_plugin
-          index_without_plugin
-          @totals = Version.calculate_totals(@versions)
-          Version.sort_versions(@versions)
+    def index
+      super
+      @totals = Version.calculate_totals(@versions)
+      Version.sort_versions(@versions)
 
-          @issues_by_version.each do |versions|
-	          versions.last.delete_if { |issue | issue.closed? }
-          end if params[:only_open]
+      @issues_by_version.each do |versions|
+        versions.last.delete_if { |issue | issue.closed? }
+      end if params[:only_open]
+    end
 
-        end
-
-        alias_method :index_without_plugin, :index
-        alias_method :index, :index_with_plugin
-
-        def show
-          @issues = @version.sorted_fixed_issues
-        end
-      
-      end
+    def show
+      @issues = @version.sorted_fixed_issues
     end
   end
 end
+
+# module AdvancedRoadmap
+#   module VersionsControllerPatch
+#     def self.included(base)
+#       base.class_eval do
+  
+#         def index_with_plugin
+#           index_without_plugin
+#           @totals = Version.calculate_totals(@versions)
+#           Version.sort_versions(@versions)
+
+#           @issues_by_version.each do |versions|
+# 	          versions.last.delete_if { |issue | issue.closed? }
+#           end if params[:only_open]
+
+#         end
+
+#         alias_method :index_without_plugin, :index
+#         alias_method :index, :index_with_plugin
+
+#         def show
+#           @issues = @version.sorted_fixed_issues
+#         end
+      
+#       end
+#     end
+#   end
+# end
