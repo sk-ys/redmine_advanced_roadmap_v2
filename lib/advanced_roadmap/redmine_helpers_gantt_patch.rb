98cc2e8fd46a6d5fab66ecd0225ae4c6a3ffce32
sk-ys <963792+sk-ys@users.noreply.github.com>
2021-02-11 07:51:40 +0900

    fix gantt

--- lib/advanced_roadmap/redmine_helpers_gantt_patch.rb
+++ lib/advanced_roadmap/redmine_helpers_gantt_patch.rb
@@ -1,10 +1,27 @@
 require_dependency "redmine/helpers/gantt"
 
+class MilestoneLabel
+  def id
+    @project_id
+  end
+  def initialize(project_id=0)
+    @project_id = project_id
+  end
+end
+
 module AdvancedRoadmap
   module RedmineHelpersGanttPatch
     def render_version(project, version, options = {})
-      if @last_rendered_project != project and project.milestones.any?
-        subject_for_milestones_label(options)
+      if @last_rendered_project_and_only != "#{project}-#{options[:only]}" and
+        project.milestones.any? and
+        options[:only] != :selected_columns
+
+        # Render milestones label
+        if @last_rendered_project != project
+          subject_for_milestones_label(options, MilestoneLabel.new(project.id))
+        end
+
+        # Render milestones
         options[:top] += options[:top_increment]
         @number_of_rows += 1
         options[:indent] += options[:indent_increment]
@@ -13,6 +30,7 @@ module AdvancedRoadmap
         end
         options[:indent] -= options[:indent_increment]
       end
+      @last_rendered_project_and_only = "#{project}-#{options[:only]}"
       @last_rendered_project = project
       super(project, version, options)
     end
@@ -25,13 +43,17 @@ module AdvancedRoadmap
       @number_of_rows += 1
     end
 
-    def subject_for_milestones_label(options)
+    def subject_for_milestones_label(options, milestone_label)
       case options[:format]
       when :html
         html_class = 'icon icon-milestones'
         s = l(:label_milestone_plural)
         subject = view.content_tag(:span, s, :class => html_class).html_safe
-        html_subject(options, subject, :css => "milestones-label")
+        if Redmine::VERSION::MAJOR >= 4
+          html_subject(options, subject, milestone_label)
+        else
+          html_subject(options, subject, :css => "milestones-label")
+        end
       when :image
         image_subject(options, l(:label_milestone_plural))
       when :pdf
@@ -46,7 +68,11 @@ module AdvancedRoadmap
         html_class = 'icon icon-milestone'
         s = view.link_to_milestone(milestone).html_safe
         subject = view.content_tag(:span, s, :class => html_class).html_safe
-        html_subject(options, subject, :css => "milestone-name")
+        if Redmine::VERSION::MAJOR >= 4
+          html_subject(options, subject, milestone)
+        else
+          html_subject(options, subject, :css => "milestone-name")
+        end
       when :image
         image_subject(options, milestone.to_s)
       when :pdf
@@ -62,30 +88,50 @@ module AdvancedRoadmap
         options[:g_width] ||= (self.date_to - self.date_from + 1) * options[:zoom]
         coords = coordinates_point(milestone.effective_date, options[:zoom])
         label = "#{h(milestone)}"
-        case options[:format]
-        when :html
-          if Redmine::VERSION::MAJOR == 2
-            html_task(options, coords, :css => "version task", :label => label, :markers => true)
-          else
-            html_task(options, coords, true, label, Version)
-          end
-        when :image
-          if Redmine::VERSION::MAJOR == 2
-            image_task(options, coords, :label => label, :markers => true, :height => 3)
-          else
-            image_task(options, coords, true, label, Version)
-          end
-        when :pdf
-          if Redmine::VERSION::MAJOR == 2
-            pdf_task(options, coords, :label => label, :markers => true, :height => 0.8)
-          else
-            pdf_task(options, coords, true, label, Version)
+        markers = true
+
+        if Redmine::VERSION::MAJOR >= 4
+          send "#{options[:format]}_task", options, coords, markers, label, milestone
+        else
+          case options[:format]
+          when :html
+            if Redmine::VERSION::MAJOR == 2
+              html_task(options, coords, :css => "version task", :label => label, :markers => true)
+            else
+              html_task(options, coords, true, label, Version)
+            end
+          when :image
+            if Redmine::VERSION::MAJOR == 2
+              image_task(options, coords, :label => label, :markers => true, :height => 3)
+            else
+              image_task(options, coords, true, label, Version)
+            end
+          when :pdf
+            if Redmine::VERSION::MAJOR == 2
+              pdf_task(options, coords, :label => label, :markers => true, :height => 0.8)
+            else
+              pdf_task(options, coords, true, label, Version)
+            end
           end
         end
       else
         ActiveRecord::Base.logger.debug "Gantt#line_for_milestone was not given a milestone with an effective_date"
         ""
       end
+    end
+
+    def number_of_rows_on_project(project)
+      count = super(project)
+      return 0 unless projects.include?(project)
+      if project.milestones.any?
+        count += 1
+        count += project.milestones.size
+      end
+    end
+
+    def html_subject(params, subject, object)
+      # TODO: need `has_children = True`, if use expander
+      super(params, subject, object)
     end
 
     private
